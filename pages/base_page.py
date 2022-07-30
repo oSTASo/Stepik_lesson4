9334a914e251d66f613a42db5f03c595e99f624f
StanislavK <staskondous@gmail.com>
2022-07-30 05:46:08 +0300

    added def is_element_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,19 @@
-class BasePage():
+from selenium.common.exceptions import NoSuchElementException
 
-    def __init__(self, browser, url):
+
+class BasePage:
+
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
