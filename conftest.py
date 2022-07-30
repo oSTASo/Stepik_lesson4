293000d835cda36c7fb90ea8c4b1348eb9d654ec
StanislavK <staskondous@gmail.com>
2022-07-30 04:56:10 +0300

    added files in lesson 4 step 6

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+
+
+@pytest.fixture
+def browser(request):
+
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("intl.accept_languages", language)
+
+    if browser_name == "chrome":
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    yield browser
+
+    browser.quit()
